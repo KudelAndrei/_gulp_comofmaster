320e71b3de8e076249d62db6b39e18099584df0d
kudelandrei <kudel1995@mail.ru>
2017-04-29 08:48:51 +0300

    add ajax form and script

--- app/js/common.js
+++ app/js/common.js
@@ -22,6 +22,22 @@ $(document).ready(function(){
 		center: true,
 	});
 
+	//E-mail Ajax Send
+	$("#callback").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
 
 	/* всплывающая форма обратной связи */
 	$('.popup-with-form').magnificPopup({
